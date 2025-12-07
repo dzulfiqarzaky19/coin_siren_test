620ce8bd0f1bd378c576c52ade21b9c69a9d458b
personal <dzulfiqarzakyaulia@gmail.com>
2025-12-08 03:17:16 +0700

    coin-siren: fix bugs on api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,13 @@
 export const getBaseUrl = () => {
+  if (process.env.VERCEL_ENV === 'production') {
+    return 'https://coin-siren-test.vercel.app';
+  }
+
+  // Preview deployments: use the auto-generated Vercel URL
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
+
+  // Local dev
   return 'http://localhost:3000';
 };
