c82fba36e034b4bcf9d623db9786515a0313610c
personal <dzulfiqarzakyaulia@gmail.com>
2025-12-08 00:36:49 +0700

    coin-siren: add button link

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,7 +1,7 @@
 import { Logo } from '../atoms/Logo';
 import { HeaderDesktopNav } from '../molecules/HeaderDesktopNav';
 import { HeaderMobileMenu } from '../molecules/HeaderMobileMenu';
-import { Typography } from '../atoms/Typography';
+import { ButtonLink } from '../atoms/ButtonLink';
 
 export const Header = () => {
   return (
@@ -9,12 +9,8 @@ export const Header = () => {
       <div className="container relative mx-auto px-4 h-20 flex items-center justify-between">
         <Logo />
         <HeaderDesktopNav />
-        <button className="hidden md:block bg-white text-[#4A77FF] px-6 py-2 rounded-lg hover:bg-opacity-90 transition-colors">
-          <Typography as="span" variant="body2" weight="bold">
-            문의하기
-          </Typography>
-        </button>
         <HeaderMobileMenu />
+        <ButtonLink label="문의하기" />
       </div>
     </header>
   );
